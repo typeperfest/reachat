f17b41756738dd9c7fcb97cc7da63224ea152a84
Вячеслав <vaceslav@MacBook-Pro-Vaceslav.local>
2022-05-27 19:48:40 +0300

    RegularButton component added

---
+++ src/components/GeneralComponents/RegularButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegularButton = (props) => {
+    return (
+        <button>
+            props.text
+        </button>
+    );
+};
+
+export default RegularButton;
