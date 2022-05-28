74ee88288141619035d12852ac930c74d0e924a8
Вячеслав <vaceslav@MacBook-Pro-Vaceslav.local>
2022-05-28 20:47:50 +0300

    NavBar component added

--- src/components/FeedPage/NavBar.jsx
+++ src/components/FeedPage/NavBar.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import RegularButton from '../GeneralComponents/RegularButton'
 
 const NavBar = () => {
+
+    function myPostsButtonClicked() {
+        console.log("My posts clicked")
+    }
+
+    function allPoststButtonClicked() {
+        console.log("All posts clicked")
+    }
+
+    function searchButtonClicked() {
+        console.log("Search clicked")
+    }
+
     return (
         <div>
-
+            <RegularButton onClick={myPostsButtonClicked} text={"My Posts"} />
+            <RegularButton onClick={allPoststButtonClicked} text={"All Posts"} />
+            <RegularButton onClick={searchButtonClicked} text={"Search"} />
         </div>
     );
 };
