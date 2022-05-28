f88d1648251da0eb9cda57c5cf7e800b64cefa50
Вячеслав <vaceslav@MacBook-Pro-Vaceslav.local>
2022-05-28 20:21:33 +0300

    added href to login page

--- src/components/GeneralComponents/RegularButton.jsx
+++ src/components/GeneralComponents/RegularButton.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const RegularButton = (props) => {
     return (
-        <button>
-            props.text
+        <button onClick={props.onClick}>
+            {props.text}
         </button>
     );
 };
