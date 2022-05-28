3a306944154ca51a669a465978dfa8dfbcd72064
Вячеслав <vaceslav@MacBook-Pro-Vaceslav.local>
2022-05-28 23:34:42 +0300

    added PostList component

--- src/components/FeedPage/PostList.jsx
+++ src/components/FeedPage/PostList.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Post from "./Post"
 
 const PostList = () => {
+
+    let [postList, setPostList] = useState([
+        {
+            id: 0,
+            author: "Author 1",
+            content: "Lorem1 ipsum dolor sit amet"
+        },
+        {
+            id: 1,
+            author: "Author 2",
+            content: "Lorem2 ipsum dolor sit amet"
+        },
+        {
+            id: 2,
+            author: "Author 3",
+            content: "Lorem3 ipsum dolor sit amet"
+        }
+    ])
+
     return (
         <div>
-
+            {postList.map(post =>
+                <Post id={post.id} author={post.author} content={post.content} />
+            )}
         </div>
     );
 };
