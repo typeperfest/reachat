da0068962bf2841f71fca2836739e59ed1e593e7
Вячеслав <vaceslav@MacBook-Pro-Vaceslav.local>
2022-05-29 18:51:10 +0300

    added internal components of NewPostModalWindow

--- src/components/FeedPage/NewPostModal/NewPostModalWindow.jsx
+++ src/components/FeedPage/NewPostModal/NewPostModalWindow.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './modal.css'
+import ConfirmDataButton from '../../GeneralComponents/ConfirmDataButton'
 
 const NewPostModalWindow = (props) => {
+
+    let inputContentRef = useRef()
+
+    function newPostAdded(e) {
+        let date = new Date();
+        let stringDate = String(date.getDate()).padStart(2, '0') + '.' + String(date.getMonth() + 1).padStart(2, '0')
+            + '.' + date.getFullYear();
+        let postObject = { author: 0, content: inputContentRef.current.value, date: stringDate}
+        console.log(postObject)
+    }
+
     return (
         <div className={props.active ? "modal active" : "modal"} onClick={() => props.setActive(false)} >
             <div className={props.active ? "modal__content active" : "modal__content"} onClick={(e) => e.stopPropagation()}>
-                {props.children}
+                <textarea ref={inputContentRef} className={"text__area"} />
+                <ConfirmDataButton text={"Add"} onClick={newPostAdded}/>
             </div>
         </div>
     );
