c181618e779199a7c7bd497b88cde51c57231566
typeperfest <perfect2111@gmail.com>
2022-05-31 12:15:47 +0300

    change NewPostModalWindow imports

--- src/components/FeedPage/NewPostModalWindow.jsx
+++ src/components/FeedPage/NewPostModalWindow.jsx
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react';
-import './styles/modal.css'
+import './styles/modalStyles.css'
 import ConfirmDataButton from '../GeneralComponents/ConfirmDataButton'
 
 const NewPostModalWindow = (props) => {
