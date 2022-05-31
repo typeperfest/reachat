85ed167cff56996f356959811a01506184a25354
typeperfest <perfect2111@gmail.com>
2022-05-31 16:59:37 +0300

    add tagline

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import UserInputForm from './GeneralComponents/UserInputForm'
 import Logo from './GeneralComponents/Logo'
 
 import './TopLevelStyles/loginStyles.css'
+import './GeneralComponents/styles/logo.css'
 
 
 const Login = () => {
@@ -10,6 +11,7 @@ const Login = () => {
     return (
         <div className={"loginForm"}>
             <Logo/>
+            <p className={"tagline"}>For everyone to see your thoughts.</p>
             <UserInputForm fieldsNamesList={[{id: 0, name: "Login"}, {id: 1, name: "Password"}]} buttonText={"Sign in"} />
             <a href="/register">Don't have an account?</a>
         </div>
