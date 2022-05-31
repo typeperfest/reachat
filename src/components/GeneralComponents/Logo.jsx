ded18b350ba4f93d55279f688fe28ce09a4293bb
typeperfest <perfect2111@gmail.com>
2022-05-31 16:53:21 +0300

    add logo

---
+++ src/components/GeneralComponents/Logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/logo.css'
+
+const Logo = () => {
+    return (
+        <div className={"logo"}>
+            <div className={"logoContent re"}>
+                re
+            </div>
+            <div className={"logoContent chat"}>
+                chat
+            </div>
+        </div>
+    );
+};
+
+export default Logo;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import UserInputForm from './GeneralComponents/UserInputForm'
+import Logo from './GeneralComponents/Logo'
+
+import './TopLevelStyles/loginStyles.css'
+
 
 const Login = () => {
 
     return (
-        <div>
+        <div className={"loginForm"}>
+            <Logo/>
             <UserInputForm fieldsNamesList={[{id: 0, name: "Login"}, {id: 1, name: "Password"}]} buttonText={"Sign in"} />
             <a href="/register">Don't have an account?</a>
         </div>
