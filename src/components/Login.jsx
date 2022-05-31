ce4bc000208c4ceddde2ce7b3735821ca635340b
typeperfest <perfect2111@gmail.com>
2022-05-31 17:17:25 +0300

    distribute form style to register form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import UserInputForm from './GeneralComponents/UserInputForm'
 import Logo from './GeneralComponents/Logo'
 
-import './TopLevelStyles/loginStyles.css'
+import './TopLevelStyles/inputFormStyles.css'
 import './GeneralComponents/styles/logo.css'
 
 
 const Login = () => {
 
     return (
-        <div className={"loginForm"}>
+        <div className={"form"}>
             <Logo/>
             <p className={"tagline"}>For everyone to see your thoughts.</p>
             <UserInputForm fieldsNamesList={[{id: 0, name: "login"}, {id: 1, name: "password"}]} buttonText={"Sign in"} />
--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import UserInputForm from './GeneralComponents/UserInputForm'
 
+import './TopLevelStyles/inputFormStyles.css'
+
 const NewUser = () => {
     return (
-        <div>
-            <UserInputForm fieldsNamesList={ [ {id: 0, name: "Login"},
-                                               {id: 1, name: "Email"},
-                                               {id: 2, name: "Password"},
-                                               {id: 3, name: "Confirm password"} ] }
+        <div className={"form"}>
+            <UserInputForm fieldsNamesList={ [ {id: 0, name: "login"},
+                                               {id: 1, name: "email"},
+                                               {id: 2, name: "password"},
+                                               {id: 3, name: "confirm password"} ] }
                            buttonText={"register"} />
         </div>
     );
