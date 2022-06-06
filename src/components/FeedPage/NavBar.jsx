b859b1d2bb6e5abd38c1f05ae58aad4c461c3c38
typeperfest <perfect2111@gmail.com>
2022-06-06 15:33:29 +0300

    changed nav bar functions

--- src/components/FeedPage/NavBar.jsx
+++ src/components/FeedPage/NavBar.jsx
@@ -12,19 +12,14 @@ const NavBar = () => {
         console.log("All posts clicked")
     }
 
-    function searchButtonClicked() {
-        console.log("Search clicked")
-    }
-
     function logOutButtonClicked() {
         console.log("Log out clicked")
     }
 
     return (
         <div className={"navBar"}>
-            <NavBarButton onClick={myPostsButtonClicked} text={"My Posts"} />
-            <NavBarButton onClick={allPoststButtonClicked} text={"All Posts"} />
-            <NavBarButton onClick={searchButtonClicked} text={"Search"} />
+            <NavBarButton onClick={myPostsButtonClicked} text={"Profile"} />
+            <NavBarButton onClick={allPoststButtonClicked} text={"Feed"} />
             <NavBarButton isFloatingRight={true} onClick={logOutButtonClicked} text={"Log out"}/>
         </div>
     );
